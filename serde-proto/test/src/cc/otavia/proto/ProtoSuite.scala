48ea7e6bc389c3738d539607cd370666955e5255
yankun <1939810907@qq.com>
2024-01-04 23:46:08 +0800

    fix(serde): unittest

--- serde-proto/test/src/cc/otavia/proto/ProtoSuite.scala
+++ serde-proto/test/src/cc/otavia/proto/ProtoSuite.scala
@@ -17,7 +17,6 @@
 package cc.otavia.proto
 
 import org.scalatest.funsuite.AnyFunSuite
-import proto.*
 
 import scala.collection.immutable.TreeMap
 
@@ -25,25 +24,6 @@ class ProtoSuite extends AnyFunSuite {
 
     test("third library ") {
         final case class VectorClock(versions: TreeMap[String, Long])
-        @RestrictedN(3, 4)
-        final case class Equipment(@N(1) id: String, @N(2) tpe: String)
-        final case class Car(id: String, color: Int, equipment: List[Equipment], vc: VectorClock)
-
-        implicit val tuple2Codec = caseCodecIdx[Tuple2[String, Long]] // codec for TreeMap[String, Long]
-
-        implicit val vectorClockCodec = caseCodecIdx[VectorClock]
-        implicit val equipmentCodec   = caseCodecAuto[Equipment]
-        implicit val carCodec         = caseCodecNums[Car](("id", 1), ("color", 4), ("equipment", 2), ("vc", 3))
-
-        val vc        = VectorClock(versions = TreeMap.empty)
-        val equipment = List(Equipment(id = "1", tpe = "123"), Equipment(id = "2", tpe = "456"))
-        val car       = Car(id = "1", color = 16416882, equipment = equipment, vc = vc)
-        // encode
-        val bytes: Array[Byte] = encode(car)
-        // decode
-        val car2: Car = decode[Car](bytes)
-
-        assert(car2 == car2)
     }
 
 }
